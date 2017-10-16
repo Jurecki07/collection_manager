fc6185fd5c4a7f0d40b905cf38a053438f248d69
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-16 16:07:00 -0500

    added get route

--- server.js
+++ server.js
@@ -28,29 +28,44 @@ let det = {
 
 //let newFootBall = new FootBall(det);
 
-newFootBall
-  .save()
-  .then(function(savedFootBall) {
-    console.log("savedBook: ", savedFootBall);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+// newFootBall
+//   .save()
+//   .then(function(savedFootBall) {
+//     console.log("savedBook: ", savedFootBall);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
 
 app.post("/footballteam", function (req, res) {
     console.log(req.body);
-    let newFootBallTeam = new Team (req.body);
+    let newFootBallTeam = new FootBall (req.body);
 
     newFootBallTeam
         .save()
         .then(function (savedFootballTeam) {
-            res.send(savedFootballTeam);})
-        .catch(function (err)
-        {res.status(500).send(err);
+            res.send(savedFootballTeam);
+        })
+        .catch(function (err) {
+            res.status(500).send(err);
         });
 
-    app.get("/footballteam/:id", function (req, res) {
+});
 
+app.get("/footballteam", function (req, res) {
+    FootBall.find()
+        .then(function (footballTeam) {
+            if (!foootballTeam) {
+                return res.send({ msg: "No Football Team Found" })
+            }
+            res.send(footballTeam)})
+        .catch(function (err) {
+            res.status(500).send(err);});
+});
+
+
+    app.get("/footballteam/:id", function (req, res) {
+FootBall.find
 
 
 
